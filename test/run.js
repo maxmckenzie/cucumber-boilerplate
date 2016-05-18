60e77a95eb6cf07450ddf0d13df444d2af775cc9
Max McKenzie <max@lemondigits.com>
2016-05-18 16:37:16 +0100

    linted test/run.js still need to replace var

--- test/run.js
+++ test/run.js
@@ -6,11 +6,11 @@
  */
 
 var spawn = require('child_process').spawn,
-    path = require('path'),
-    args = [path.join(__dirname, '..', 'node_modules', 'mocha', 'bin', '_mocha')],
-    config = require('./config').config,
-    flag,
-    proc;
+  path = require('path'),
+  args = [path.join(__dirname, '..', 'node_modules', 'mocha', 'bin', '_mocha')],
+  config = require('./config').config,
+  flag,
+  proc;
 
 /**
  * execute init script with mocha
@@ -22,62 +22,62 @@ args.push(path.join(__dirname, 'support', 'init.js'));
  * set mocha configs
  */
 for (flag in (config.mochaOpts || {})) {
-    args.push('--' + flag + '=' + config.mochaOpts[flag]);
+  args.push('--' + flag + '=' + config.mochaOpts[flag]);
 }
 
-process.argv.slice(2).forEach(function (arg) {
-    var flag;
+process.argv.slice(2).forEach((arg) => {
+  var flag;
 
-    /**
-     * set mocha specific args
-     */
-    flag = arg.split('=')[0];
-    switch (flag) {
-        case '-d':
-            args.unshift('--debug');
-            break;
-        case 'debug':
-        case '--debug':
-        case '--debug-brk':
-            args.unshift(arg);
-            break;
-        case '-gc':
-        case '--expose-gc':
-            args.unshift('--expose-gc');
-            break;
-        case '--gc-global':
-        case '--harmony':
-        case '--harmony-proxies':
-        case '--harmony-collections':
-        case '--harmony-generators':
-        case '--prof':
-            args.unshift(arg);
-            break;
-        default:
-            if (0 === arg.indexOf('--trace')) {
-                args.unshift(arg);
-            } else {
-                args.push(arg);
-            }
-            break;
-    }
+  /**
+   * set mocha specific args
+   */
+  flag = arg.split('=')[0];
+  switch (flag) {
+    case '-d':
+      args.unshift('--debug');
+      break;
+    case 'debug':
+    case '--debug':
+    case '--debug-brk':
+      args.unshift(arg);
+      break;
+    case '-gc':
+    case '--expose-gc':
+      args.unshift('--expose-gc');
+      break;
+    case '--gc-global':
+    case '--harmony':
+    case '--harmony-proxies':
+    case '--harmony-collections':
+    case '--harmony-generators':
+    case '--prof':
+      args.unshift(arg);
+      break;
+    default:
+      if (arg.indexOf('--trace') === 0) {
+        args.unshift(arg);
+      } else {
+        args.push(arg);
+      }
+      break;
+  }
 });
 
 proc = spawn(process.argv[0], args, {
-    stdio: 'inherit'
+  stdio: 'inherit',
 });
 proc.on(
-    'exit',
-    function (code, signal) {
-        process.on(
-            'exit',
-            function () {
-                if (signal) {
-                    process.kill(process.pid, signal);
-                } else {
-                    process.exit(code);
-                }
-            }
-        );
-    }
+  'exit',
+  (code, signal) => {
+    process.on(
+      'exit',
+      () => {
+        if (signal) {
+          process.kill(process.pid, signal);
+        } else {
+          process.exit(code);
+        }
+      }
+    );
+  }
 );
